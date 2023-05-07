c96cfd5345382056d94c8764a9062eaa09071b39
VeraMarija <vg48765@student.oss.unist.hr>
2023-05-07 19:31:08 +0200

    started vjezba5

---
+++ c-sharp-lab2023-VeraMarijaGuja/vjezba5/Models/PatientContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace vjezba5.Models
+{
+    public class PatientContext : DbContext
+    {
+        public PatientContext(DbContextOptions<PatientContext> options) : base(options) 
+        {
+
+        }
+
+        public DbSet<PatientModel> Patients { get; set; }
+
+    }
+}
---
+++ c-sharp-lab2023-VeraMarijaGuja/vjezba5/Models/PatientModel.cs
@@ -0,0 +1,15 @@
+﻿namespace vjezba5.Models
+{
+    public class PatientModel
+    {
+        public string Oib { get; set; }
+        public string Mbo { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime? Dob { get; set; }
+        public string? Gender { get; set; }
+        public string? MedicalDiagnosis { get; set; }
+        public string? Insurance { get; set; }
+        public string? Status { get; set; }
+    }
+}
